5718f1164ff7af29971fee13ee4f7c0e4c12343e
AsfandRanglerz <asfand.ranglerz@gmail.com>
2021-10-17 04:11:44 -0700

    changes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,3 +7,18 @@
 
 "use strict";
 
+$(function () {
+    $('#checkbox').click(function(){
+        var element = document.body;
+        element.classList.toggle("dark");
+    });
+    if ($(window).scrollTop() == 0) {
+        $('.theme-white .navbar').removeClass('scrollBgNav');
+    }
+    $(window).on('scroll', function () {
+        $('.theme-white .navbar').addClass('scrollBgNav');
+        if ($(window).scrollTop() == 0) {
+            $('.theme-white .navbar').removeClass('scrollBgNav');
+        }
+    });
+});
